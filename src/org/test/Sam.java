5ada84148a416fc991bce88715f52889118fd4f9
sindhu1114 <sindhumanu1114@gmail.com>
2018-10-25 12:20:36 +0530

    i can

--- src/org/test/Sam.java
+++ src/org/test/Sam.java
@@ -3,5 +3,10 @@ package org.test;
 public class Sam {
 public static void main(String[] args) {
 	System.out.println("i am good boy");
+	System.out.println("i will select in the interview");
+}
+public void get() {
+	System.out.println("hi");
+
 }
 }
